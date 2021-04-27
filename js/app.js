0dfb3ea4597b9f1b0ab0be8e484afde7bebb53fe
Mathius <devachievem@gmail.com>
2021-04-27 15:54:01 -0700

    implement pulsing button, on hover icon color change

--- js/app.js
+++ js/app.js
@@ -262,3 +262,22 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(window).scroll(function () {
+    $('.highlight').each(function () {
+        console.log('fun is scrolled!!!');
+        if (isScrolledIntoView(this) === true) {
+            $(this).addClass('highlight-transition');
+        }
+    });
+});
